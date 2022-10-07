b28114f466327b3d5636ef2497cba230b7ec65de
Cozmu <www.jorgereis@gmail.com>
2022-10-07 19:27:11 -0300

    Finalizado Requisito 5

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { getCategories } from '../services/api';
+import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
 
 class Search extends React.Component {
   constructor() {
@@ -8,40 +8,85 @@ class Search extends React.Component {
 
     this.state = {
       category: [],
+      query: '',
+      products: [],
     };
   }
 
   async componentDidMount() {
     const newRequest = await getCategories();
     this.setState({ category: newRequest });
-    console.log(newRequest);
   }
 
+  onInputChange = ({ target: { value, name } }) => {
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleClick = async () => {
+    const { query } = this.state;
+    const request = await getProductsFromCategoryAndQuery(null, query);
+    console.log(request.results);
+    this.setState({
+      products: request.results,
+    });
+  };
+
   render() {
-    const { category } = this.state;
+    const { category, query, products } = this.state;
     return (
       <>
+        <nav>
+          <NavLink to="/cart" data-testid="shopping-cart-button">Pesquisar</NavLink>
+        </nav>
         <div>
           <p data-testid="home-initial-message">
             Digite algum termo de pesquisa ou escolha uma categoria.
           </p>
         </div>
 
-        <nav>
-          <NavLink to="/cart" data-testid="shopping-cart-button">Pesquisar</NavLink>
-        </nav>
         <div>
           { category.map((element, index) => (
             <label htmlFor={ element.id } data-testid="category" key={ index }>
-              { element.name }
               <input
                 type="radio"
                 value={ element.name }
                 id={ element.id }
               />
-
+              { element.name }
             </label>
           )) }
+        </div>
+        <div>
+          <label htmlFor="query">
+            <input
+              type="text"
+              name="query"
+              id="query"
+              value={ query }
+              onChange={ this.onInputChange }
+              data-testid="query-input"
+            />
+          </label>
+          <button
+            type="button"
+            onClick={ this.handleClick }
+            data-testid="query-button"
+          >
+            pesquisar
+          </button>
+          { products.length === 0 ? <p>Nenhum produto foi encontrado</p> : (
+            products.map((element, i) => (
+              <div
+                data-testid="product"
+                key={ i }
+              >
+                <img src={ element.thumbnail } alt={ element.id } />
+                <p>{element.title}</p>
+                <p>{element.price}</p>
+              </div>
+            ))) }
         </div>
       </>
 
