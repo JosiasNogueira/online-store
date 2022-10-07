cc590232ea23f998f371735e0019e16008e7b994
Pablo Yuri <merroxpy@gmail.com>
2022-10-07 17:20:45 -0300

    Finalizado requisito 3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Cart from './components/Cart';
 import Search from './components/Search';
 
 class App extends React.Component {
@@ -8,6 +9,7 @@ class App extends React.Component {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={ Search } />
+          <Route exact path="/cart" component={ Cart } />
         </Switch>
       </BrowserRouter>
     );
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 class Search extends React.Component {
   render() {
     return (
-      <div>
-        <p data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </p>
-      </div>
+      <>
+        <div>
+          <p data-testid="home-initial-message">
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </p>
+        </div>
+
+        <nav>
+          <NavLink to="/cart" data-testid="shopping-cart-button">Pesquisar</NavLink>
+        </nav>
+      </>
+
     );
   }
 }
